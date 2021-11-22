cf0ab7ffe505a468f285c25bc7c620f211f4f40f
mrLuisFer <mrluisfeer@gmail.com>
2021-11-22 09:09:52 -0600

    add some basic config for treesitter

--- lua/plugins/treesitter-config.lua
+++ lua/plugins/treesitter-config.lua
@@ -1,1 +1,29 @@
-require'nvim-treesitter.configs'.setup{}
+require'nvim-treesitter.configs'.setup{
+	ensure_installed = {
+		'bash', 
+		'css', 
+		'dart', 
+		'dockerfile', 
+		'fish', 
+		'go', 
+		'gomod', 
+		'graphql',
+		'html',
+		'javascript',
+		'json',
+		'lua',
+		'php',
+		'python',
+		'rust',
+		'scss',
+		'svelte',
+		'toml',
+		'tsx',
+		'typescript',
+		'vim',
+		'yaml',
+	},
+	highlight = {
+  	enable = true,
+  }
+}
