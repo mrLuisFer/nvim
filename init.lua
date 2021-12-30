570cce189688954514b23cdd85eddb410cbceb04
Luis Alvarez <mrluisfeer@gmail.com>
2021-12-30 01:21:05 -0600

    Setup/lua config (#5)
    * change init of vim script to lua
    
    * add basic options for vim
    
    * add some useful maps
    
    * add coloscheme option
    
    * sync plugins and add barbar
    
    * add basic barbar setup
    
    * change relative number for normal number
    
    * add fancy vim-notify and fix indent
    
    * add notify module

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- INIT CONFIG
+require('options')
+require('mappings')
+
+require("plugins")
+require("plugins/colorizer-config")
+require("plugins/dashboard-config")
+require("plugins/lualine-config")
+require("plugins/treelua-config")
+require("plugins/cmp-config")
+require("plugins/treesitter-config")
+require("plugins/barbar-config")
+
---
+++ lua/options/init.lua
@@ -0,0 +1,44 @@
+local set = vim.opt
+local g = vim.g
+
+set.expandtab = true
+set.smarttab = true
+set.shiftwidth = 4
+set.tabstop = 4
+-- set.backspace = 2
+set.showtabline = 2
+
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.termguicolors = true
+set.showmode = false
+set.splitbelow = true
+set.splitright = true
+set.wrap = false
+set.scrolloff = 5
+set.fileencoding = 'utf-8'
+
+set.number = true
+set.cursorline = true
+set.wildmenu = true
+set.completeopt = 'menuone,noselect'
+set.title = true
+
+set.showcmd = true
+set.hidden = true
+set.mouse = "a"
+set.ruler = true
+
+set.clipboard = 'unnamed'
+
+-- Vim Smoothie
+g.smoothie_enabled = 1
+
+-- Colorscheme
+vim.cmd('colorscheme onedark')
+
+vim.notify = require('notify')
+
