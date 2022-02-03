d5a3638c6450b92c7b301ba1842fbcbd4978abdb
mrLuisFer <mrluisfeer@gmail.com>
2022-02-02 22:45:33 -0600

    add some colorschemes

--- lua/options/themes.lua
+++ lua/options/themes.lua
@@ -1,6 +1,12 @@
 local cmd = vim.cmd
 
+--To access the available color schemes, you can type:
+--And type :colorscheme space and press TAB
+
 -- Colorscheme
 -- cmd('colorscheme onedark')
-cmd('colorscheme gruvbox')
+-- cmd('colorscheme gruvbox')
+-- cmd('colorscheme night-owl')
+-- cmd('colorscheme molokai')
+cmd('colorscheme palenight')
 
