7bd38ab50be730eb5ebd58cecbfe9e428e60fbb0
mrLuisFer <mrluisfeer@gmail.com>
2022-02-28 21:53:07 -0600

    remove unused colorscheme

--- lua/options/themes.lua
+++ lua/options/themes.lua
@@ -1,5 +1,4 @@
 local cmd = vim.cmd
-
 --To access the available color schemes, you can type:
 --And type :colorscheme space and press TAB
 
@@ -7,6 +6,5 @@ local cmd = vim.cmd
 -- cmd('colorscheme onedark')
 -- cmd('colorscheme gruvbox')
 -- cmd('colorscheme night-owl')
--- cmd('colorscheme molokai')
  cmd('colorscheme palenight')
 
