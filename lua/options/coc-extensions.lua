152c552caec809e636a89ccb4815d7a6eef0e3ca
mrLuisFer <mrluisfeer@gmail.com>
2022-04-19 20:26:03 -0500

    remove lua plugins from CoC

--- lua/options/coc-extensions.lua
+++ lua/options/coc-extensions.lua
@@ -1,4 +1,4 @@
 local g = vim.g
 
-g.coc_global_extensions = {'coc-json', 'coc-git', 'coc-css', 'coc-browser', 'coc-cssmodules', 'coc-deno', 'coc-discord', 'coc-eslint', 'coc-go', 'coc-html', 'coc-java', 'coc-phpls', 'coc-prettier', 'coc-pyright', 'coc-rust-analyzer', 'coc-svelte', 'coc-tsserver', 'coc-vetur', 'coc-vimlsp', 'coc-stylua', 'coc-sumneko-lua'}
+g.coc_global_extensions = {'coc-json', 'coc-git', 'coc-css', 'coc-browser', 'coc-cssmodules', 'coc-deno', 'coc-discord', 'coc-eslint', 'coc-go', 'coc-html', 'coc-java', 'coc-phpls', 'coc-prettier', 'coc-pyright', 'coc-rust-analyzer', 'coc-svelte', 'coc-tsserver', 'coc-vetur', 'coc-vimlsp'}
 
