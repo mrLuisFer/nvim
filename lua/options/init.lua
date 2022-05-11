904db6bcee07e0653950027e9a3ba18c6bb6a193
mrLuisFer <mrluisfeer@gmail.com>
2022-05-10 21:44:21 -0500

    remove cursorline option

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -6,31 +6,25 @@ set.smarttab = true
 set.shiftwidth = 2
 set.tabstop = 2
 set.showtabline = 2
-
 set.hlsearch = true
 set.incsearch = true
 set.ignorecase = true
 set.smartcase = true
-set.visualbell = true
-
+set.visualbell = false
 set.termguicolors = true
 set.showmode = false
 set.splitbelow = true
 set.splitright = true
 set.wrap = false
-set.scrolloff = 5
+set.scrolloff = 3
 set.fileencoding = 'utf-8'
-
 set.number = true
-set.cursorline = true
 set.wildmenu = true
 set.completeopt = 'menuone,noselect'
 set.title = true
-
 set.showcmd = true
 set.hidden = true
 set.mouse = "a"
 set.ruler = true
-
 set.clipboard = 'unnamed'
 
