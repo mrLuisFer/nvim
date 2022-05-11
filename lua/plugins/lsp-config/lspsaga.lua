d5a9adc497f1ba28248f2f24a882b83e668db3f9
mrLuisFer <mrluisfeer@gmail.com>
2022-05-10 21:41:50 -0500

    remove unused options

--- lua/plugins/lsp-config/lspsaga.lua
+++ lua/plugins/lsp-config/lspsaga.lua
@@ -1,13 +1,11 @@
 require'lspsaga'.setup {
     debug = false,
     use_saga_diagnostic_sign = true,
-    -- diagnostic sign
     error_sign = "",
     warn_sign = "",
     hint_sign = "",
     infor_sign = "",
     diagnostic_header_icon = "   ",
-    -- code action title icon
     code_action_icon = " ",
     code_action_prompt = {
     enable = true,
@@ -37,7 +35,6 @@ require'lspsaga'.setup {
     definition_preview_icon = "  ",
     border_style = "single",
     rename_prompt_prefix = "➤",
-    server_filetype_map = {},
     diagnostic_prefix_format = "%d. ",
 }
 
