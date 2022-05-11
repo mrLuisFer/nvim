ee1ac52fa1135c161af17550a02ad47eb0ed33b3
mrLuisFer <mrluisfeer@gmail.com>
2022-05-10 21:40:09 -0500

    remove unused coc extensions

--- lua/options/coc-extensions.lua
+++ lua/options/coc-extensions.lua
@@ -1,4 +1,3 @@
 local g = vim.g
-
-g.coc_global_extensions = {'coc-json', 'coc-git', 'coc-css', 'coc-browser', 'coc-cssmodules', 'coc-deno', 'coc-eslint', 'coc-go', 'coc-html', 'coc-java', 'coc-phpls', 'coc-prettier', 'coc-pyright', 'coc-rust-analyzer', 'coc-svelte', 'coc-tsserver', 'coc-vetur', 'coc-vimlsp'}
+g.coc_global_extensions = {'coc-json', 'coc-git', 'coc-css', 'coc-browser', 'coc-cssmodules', 'coc-deno', 'coc-eslint', 'coc-go', 'coc-html', 'coc-pyright', 'coc-rust-analyzer', 'coc-svelte', 'coc-tsserver', 'coc-vetur', 'coc-vimlsp'}
 
