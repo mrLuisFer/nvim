f4f41d3715ec504b8ba25dc6bfd32e07e4925c82
mrLuisFer <mrluisfeer@gmail.com>
2022-05-26 09:52:50 -0500

    add coc-java for a syntax support

--- lua/options/coc-extensions.lua
+++ lua/options/coc-extensions.lua
@@ -1,3 +1,3 @@
 local g = vim.g
-g.coc_global_extensions = {'coc-json', 'coc-git', 'coc-css', 'coc-browser', 'coc-cssmodules', 'coc-deno', 'coc-eslint', 'coc-go', 'coc-html', 'coc-pyright', 'coc-rust-analyzer', 'coc-svelte', 'coc-tsserver', 'coc-vetur', 'coc-vimlsp'}
+g.coc_global_extensions = {'coc-java', 'coc-json', 'coc-git', 'coc-css', 'coc-browser', 'coc-cssmodules', 'coc-deno', 'coc-eslint', 'coc-go', 'coc-html', 'coc-pyright', 'coc-rust-analyzer', 'coc-svelte', 'coc-tsserver', 'coc-vetur', 'coc-vimlsp'}
 
