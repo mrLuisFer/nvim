20983ee97a6e25daa775d16738e7d74cafb153b0
mrLuisFer <mrluisfeer@gmail.com>
2022-05-26 00:03:48 -0500

    change default theme

--- lua/options/themes.lua
+++ lua/options/themes.lua
@@ -2,8 +2,8 @@ local cmd = vim.cmd
 --To access the available color schemes, you can type:
 --And type :colorscheme space and press TAB
 -- cmd('colorscheme onedark')
-cmd('colorscheme gruvbox')
+-- cmd('colorscheme gruvbox')
 -- cmd('colorscheme night-owl')
 -- cmd('colorscheme palenight')
--- cmd('colorscheme tokyonight')
+cmd('colorscheme tokyonight')
 
