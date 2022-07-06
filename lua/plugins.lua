0cd8f97813d8c54f3a6cf7f98d2e599ff0a36ec1
mrLuisFer <mrluisfeer@gmail.com>
2022-07-05 22:17:23 -0500

    update packer syntax

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,31 +20,31 @@ end
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 	-- THEMES:
-	use {'navarasu/onedark.nvim', as = 'onedark'}
-	use 'morhetz/gruvbox'
+	use { 'navarasu/onedark.nvim', as = 'onedark' }
+	use { 'morhetz/gruvbox'}
   use { 'folke/tokyonight.nvim', branch =  'main' }
   use {'catppuccin/nvim', as = 'catppuccin'}
-	use 'marko-cerovac/material.nvim'
-	use {'overcache/NeoSolarized', as = 'neosolarized'}
-	use 'glepnir/zephyr-nvim'
+	use { 'marko-cerovac/material.nvim' }
+	use {'overcache/NeoSolarized', as = 'neosolarized' }
+	use { 'glepnir/zephyr-nvim' }
 	-- PLUGINS:
-  use 'kyazdani42/nvim-web-devicons'
-	use 'ryanoasis/vim-devicons'
-	use 'christoomey/vim-tmux-navigator' -- Navigate with C-h C-l C-j C-k
-	use {'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
-	use 'junegunn/fzf.vim'
-	use 'neovim/nvim-lspconfig'
-  use {'akinsho/bufferline.nvim', tag = "v2.*"}
-	use	'editorconfig/editorconfig-vim'
-  use 'mhinz/vim-signify'
-	use {'prettier/vim-prettier',	run = 'yarn install --frozen-lockfile --production'}
-	use 'williamboman/nvim-lsp-installer'
-  use 'tami5/lspsaga.nvim'
-  use 'onsails/lspkind-nvim'
-  use 'psliwka/vim-smoothie'
-  use 'tpope/vim-commentary'
-  use 'preservim/nerdcommenter'
-  use {'kyazdani42/nvim-tree.lua', tag = 'nightly'}
+  use { 'kyazdani42/nvim-web-devicons' }
+	use { 'ryanoasis/vim-devicons' }
+	use { 'christoomey/vim-tmux-navigator' } -- Navigate with C-h C-l C-j C-k
+	use { 'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
+	use { 'junegunn/fzf.vim' }
+	use { 'neovim/nvim-lspconfig' }
+  use { 'akinsho/bufferline.nvim', tag = "v2.*" }
+	use	{ 'editorconfig/editorconfig-vim' }
+  use { 'mhinz/vim-signify' }
+	use { 'prettier/vim-prettier',	run = 'yarn install --frozen-lockfile --production' }
+	use { 'williamboman/nvim-lsp-installer' }
+  use { 'tami5/lspsaga.nvim' }
+  use { 'onsails/lspkind-nvim' }
+  use { 'psliwka/vim-smoothie' }
+  use { 'tpope/vim-commentary' }
+  use { 'preservim/nerdcommenter' }
+  use { 'kyazdani42/nvim-tree.lua', tag = 'nightly' }
 	use { 'nvim-telescope/telescope.nvim',
     requires = {
       { 'nvim-lua/popup.nvim' },
@@ -52,25 +52,25 @@ return require('packer').startup(function()
       { 'nvim-telescope/telescope-fzf-native.nvim' }
     }
   }
-	use 'nvim-lua/popup.nvim'
-	use 'windwp/nvim-ts-autotag'
-  use {'norcalli/nvim-colorizer.lua', config = function () require('colorizer').setup() end}
-  use {"lukas-reineke/indent-blankline.nvim", config = function() require("indent_blankline").setup { filetype_exclude = { "dashboard" } } end}
-	use {'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-	use {'nvim-treesitter/nvim-treesitter'}
-	use {"windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end}
-	use {"mcauley-penney/tidy.nvim", config = function() require("tidy").setup() end	}
-	use {'mhartington/formatter.nvim' }
-	use 'sbdchd/neoformat'
+	use { 'nvim-lua/popup.nvim' }
+	use { 'windwp/nvim-ts-autotag' }
+  use { 'norcalli/nvim-colorizer.lua', config = function () require('colorizer').setup() end}
+  use { "lukas-reineke/indent-blankline.nvim", config = function() require("indent_blankline").setup { filetype_exclude = { "dashboard" } } end }
+	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+	use { 'nvim-treesitter/nvim-treesitter'}
+	use { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end }
+	use { "mcauley-penney/tidy.nvim", config = function() require("tidy").setup() end	}
+	use { 'mhartington/formatter.nvim' }
+	use { 'sbdchd/neoformat' }
 	use {	'fatih/vim-go', run = ':GoUpdateBinaries' }
-	use	{'styled-components/vim-styled-components', branch = 'main' }
-	use {'groenewege/vim-less', as = 'less' }
-	use {'kchmck/vim-coffee-script', as = 'coffee' }
-	use 'folke/lsp-colors.nvim'
-	use 'xiyaowong/nvim-transparent'
+	use	{ 'styled-components/vim-styled-components', branch = 'main' }
+	use { 'groenewege/vim-less', as = 'less' }
+	use { 'kchmck/vim-coffee-script', as = 'coffee' }
+	use { 'folke/lsp-colors.nvim' }
+	use { 'xiyaowong/nvim-transparent' }
 	use { 'nathom/filetype.nvim' }
 	use { 'lewis6991/impatient.nvim' }
-	use 'jose-elias-alvarez/typescript.nvim'
+	use { 'jose-elias-alvarez/typescript.nvim' }
 	-- CMP:
   use { 'hrsh7th/nvim-cmp' }
   use { 'hrsh7th/cmp-nvim-lua' }
@@ -81,11 +81,11 @@ return require('packer').startup(function()
   use { 'hrsh7th/cmp-calc' }
   use { 'David-Kunz/cmp-npm' }
   use { 'saadparwaiz1/cmp_luasnip' }
-	use 'L3MON4D3/LuaSnip'
+	use { 'L3MON4D3/LuaSnip' }
 	use { 'mg979/vim-visual-multi' }
 	use { 'p00f/nvim-ts-rainbow' }
 	-- if you want to use GitHub Copilot, enable this plugin and run :Copilot setup
-	-- use 'github/copilot.vim'
+	-- use { 'github/copilot.vim' }
 	if packer_bootstrap then
 		require('packer').sync()
   end
