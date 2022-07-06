ca1ae6f60f3f83a5233f3f0c8e9ac98a91e8d05c
mrLuisFer <mrluisfeer@gmail.com>
2022-07-05 22:02:09 -0500

    update options using an object with a for loop

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -1,40 +1,78 @@
-local set = vim.opt
-local g = vim.g
-local fn = vim.fn
+local g   =   vim.g
+local fn  =   vim.fn
+local opt =   vim.opt
+local g   =   vim.g
 
-set.expandtab = true
-set.smarttab = true
-set.shiftwidth = 2
-set.tabstop = 2
-set.showtabline = 2
-set.hlsearch = true
-set.incsearch = true
-set.ignorecase = true
-set.smartcase = true
-set.visualbell = false
-set.termguicolors = true
-set.showmode = false
-set.splitbelow = true
-set.splitright = true
-set.wrap = false
-set.scrolloff = 3
-set.fileencoding = 'utf-8'
-set.number = true
-set.wildmenu = true
-set.completeopt = 'menuone,noselect'
-set.title = true
-set.showcmd = true
-set.hidden = true
-set.mouse = "a"
-set.ruler = true
-set.clipboard = 'unnamed'
-set.relativenumber = true
-set.cursorline = true
+local options = {
+	ruler					 = true,
+  clipboard      = "unnamed,unnamedplus",   --- Copy-paste between vim and everything else
+  cmdheight      = 2,                       --- Give more space for displaying messages
+  completeopt    = "menu,menuone,noselect", --- Better autocompletion
+  cursorline     = true,                    --- Highlight of current line
+  emoji          = false,                   --- Fix emoji display
+  expandtab      = true,                    --- Use spaces instead of tabs
+  foldlevelstart = 99,                      --- Expand all folds by default
+  foldtext       = "CustomFold()",          --- Emit custom function for foldtext
+  ignorecase     = true,                    --- Needed for smartcase
+  laststatus     = 3,                       --- Have a global statusline at the bottom instead of one for each window
+  lazyredraw     = true,                    --- Makes macros faster & prevent errors in complicated mappings
+  mouse          = "a",                     --- Enable mouse
+  number         = true,                    --- Shows current line number
+  pumheight      = 10,                      --- Max num of items in completion menu
+  relativenumber = true,                    --- Enables relative number
+  scrolloff      = 8,                       --- Always keep space when scrolling to bottom/top edge
+  shiftwidth     = 2,                       --- Change a number of space characeters inseted for indentation
+  showtabline    = 2,                       --- Always show tabs
+  signcolumn     = "yes:2",                 --- Add extra sign column next to line number
+  smartcase      = true,                    --- Uses case in search
+  smartindent    = true,                    --- Makes indenting smart
+  smarttab       = true,                    --- Makes tabbing smarter will realize you have 2 vs 4
+  softtabstop    = 2,                       --- Insert 2 spaces for a tab
+  splitright     = true,                    --- Vertical splits will automatically be to the right
+  swapfile       = false,                   --- Swap not needed
+  tabstop        = 2,                       --- Insert 2 spaces for a tab
+  termguicolors  = true,                    --- Correct terminal colors
+  timeoutlen     = 200,                     --- Faster completion (cannot be lower than 200 because then commenting doesn't work)
+  undofile       = true,                    --- Sets undo to file
+  updatetime     = 100,                     --- Faster completion
+  viminfo        = "'1000",                 --- Increase the size of file history
+  wildignore     = "*node_modules/**",      --- Don't search inside Node.js modules (works for gutentag)
+  wrap           = false,                   --- Display long lines as just one line
+  writebackup    = false,                   --- Not needed
+  -- Neovim defaults
+  autoindent     = true,                    --- Good auto indent
+  backspace      = "indent,eol,start",      --- Making sure backspace works
+  backup         = false,                   --- Recommended by coc
+  conceallevel   = 0,                       --- Show `` in markdown files
+  encoding       = "utf-8",                 --- The encoding displayed
+  errorbells     = false,                   --- Disables sound effect for errors
+  fileencoding   = "utf-8",                 --- The encoding written to file
+  incsearch      = true,                    --- Start searching before pressing enter
+  showmode       = false,                   --- Don't show things like -- INSERT -- anymore
+}
+
+local globals = {
+  fillchars                   = "fold:\\ ", --- Fill chars needed for folds
+  mapleader                   = ' ',        --- Map leader key to SPC
+  speeddating_no_mappings     = 1,          --- Disable default mappings for speeddating
+	vim_svelte_plugin_load_full_syntax = 1,
+	neoline_disable_tabline = 1
+}
+
+opt.shortmess:append('c');
+opt.formatoptions:remove('c');
+opt.formatoptions:remove('r');
+opt.formatoptions:remove('o');
+
+for k, v in pairs(options) do
+  opt[k] = v
+end
+
+for k, v in pairs(globals) do
+  g[k] = v
+end
 
 -- Global Plugins variable
-g.vim_svelte_plugin_load_full_syntax = 1
-g.neoline_disable_tabline = 1
-
 if fn.has('linux') or fn.has('unix') then
   g.python_host_prog = '/usr/bin/python3'
 elseif fn.has('win32') or fn.has('win64') then
