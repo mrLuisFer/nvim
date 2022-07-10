553a8afd3577c7a5b0e5c3a12a6fc93e5852f89a
mrLuisFer <mrluisfeer@gmail.com>
2022-07-10 08:11:22 -0500

    update null-ls config

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -3,6 +3,18 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local action = null_ls.builtins.code_actions
 
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
 	debug = false,
 	sources = {
@@ -14,17 +26,22 @@ null_ls.setup({
 		formatting.stylua,
 		formatting.isort,
 		formatting.codespell.with({ filetypes = { "markdown" } }),
-
 		-- diagnostics
 		diagnostics.eslint,
 		-- actions
 		action.eslint,
 	},
 	on_attach = function(client, bufnr)
-		if client.server_capabilities.document_formatting then
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
+		local bufcmd = vim.api.nvim_buf_create_user_command
+		local format = function()
+			local params = vim.lsp.util.make_formatting_params({})
+			client.request("textDocument/formatting", params, nil, bufnr)
 		end
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+		if client.server_capabilities.documentFormattingProvider then
+			bufcmd(bufnr, "NullFormat", format, { desc = "Format using null-ls" })
+		end
+
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
@@ -32,15 +49,11 @@ null_ls.setup({
 				buffer = bufnr,
 				callback = function()
 					if vim.fn.has("nvim-0.8") == 1 then
-						vim.lsp.buf.format({
-							timeout_ms = 2000, -- 2 seconds
-							bufnr = bufnr,
-							filter = function()
-								return client.name == "null-ls"
-							end,
-						})
+						vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
+						lsp_formatting(bufnr)
+					else
+						vim.lsp.buf.formatting_sync()
 					end
-					vim.lsp.buf.formatting_sync(nil, 2000) -- 2 seconds
 				end,
 			})
 		end
