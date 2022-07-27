45b56afcebe6189db81416b685c0a36612ef984e
mrLuisFer <mrluisfeer@gmail.com>
2022-07-26 23:28:29 -0500

    move all cmd into an cmd func and remove winbar

--- lua/options/autocmds.lua
+++ lua/options/autocmds.lua
@@ -1,8 +1,5 @@
 local api = vim.api
 
--- Auto sync plugins on save of plugins.lua
--- api.nvim_create_autocmd("BufWritePost", { pattern = "plugins.lua", command = "source <afile> | PackerSync" })
-
 -- Highlight on yank
 api.nvim_create_autocmd("TextYankPost",
   { callback = function() vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 }) end })
@@ -14,52 +11,3 @@ api.nvim_create_autocmd("BufNewFile", { pattern = "*/node_modules/*", command = 
 -- Enable spell checking for certain file types
 api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "*.tex" },
   command = "setlocal spell" })
-
--- Winbar (for nvim 0.8+)
-if vim.fn.has('nvim-0.8') == 1 then
-  api.nvim_create_autocmd({ "CursorMoved", "BufWinEnter", "BufFilePost" }, {
-    callback = function()
-      local winbar_filetype_exclude = {
-        "help",
-        "startify",
-        "dashboard",
-        "packer",
-        "neogitstatus",
-        "NvimTree",
-        "Trouble",
-        "alpha",
-        "lir",
-        "Outline",
-        "spectre_panel",
-        "toggleterm",
-        "TelescopePrompt",
-        "DressingInput",
-        "DressingSelect",
-        "neotest-summary",
-      }
-
-      if (api.nvim_win_get_config(0).relative ~= "") then
-        return
-      end
-
-      if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
-        vim.opt_local.winbar = nil
-        return
-      end
-
-      if vim.bo.filetype == 'GitBlame' then
-        local hl_group = "Secondary"
-        vim.opt_local.winbar = " " .. "%#" .. hl_group .. "#" .. require('icons').git .. "Blame" .. "%*"
-        return
-      end
-
-      local present, winbar = pcall(require, "winbar")
-      if not present or type(winbar) == "boolean" then
-        vim.opt_local.winbar = nil
-        return
-      end
-
-      vim.opt_local.winbar = winbar.filename()
-    end,
-  })
-end
--- lua/options/filetypes.lua
+++ lua/options/filetypes.lua
@@ -24,17 +24,16 @@ filetype.add {
 cmd([[
 	filetype indent plugin on
 	syntax enable
-]])
-cmd([[ au BufEnter * set fo-=c fo-=r fo-=o ]])
-cmd([[ autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0 ]])
-cmd([[
+
+  au BufEnter * set fo-=c fo-=r fo-=o
+
+  autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0
+
 	autocmd FileType html setlocal tabstop=2 shiftwidth=2 softtabstop=blinkon250
 	autocmd FileType css setlocal tabstop=2 shiftwidth=2 softtabstop=2
 	autocmd FileType javascriptreact setlocal tabstop=2 shiftwidth=2 softtabstop=2
 	autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 softtabstop=2
 	autocmd FileType lua setlocal tabstop=2 shiftwidth=2 softtabstop=2
-]])
-cmd([[
-  " Add `:Format` command to format current buffer.
+
   command! -nargs=0 Format :call CocActionAsync('format')
 ]])
