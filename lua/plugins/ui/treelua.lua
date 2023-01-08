f67ef504201778307e49e0a1be5cf8cf2c63bd09
Luis Alvarez <luis.alvarez@nowports.com>
2023-01-08 11:14:42 -0600

    update to get current buffer in the directory three

--- lua/plugins/ui/treelua.lua
+++ lua/plugins/ui/treelua.lua
@@ -3,6 +3,11 @@ vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+    ignore_list = {},
+  },
   view = {
     adaptive_size = true,
     mappings = {
